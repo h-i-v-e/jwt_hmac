f19c91e6e30f569a9f56ec65774260e369c553f7
Jerome Johnson <jerome.johnson@aotea.co.nz>
2021-12-07 09:49:16 +1300

    Added some documentation and changed the key from &str to &[u8]

--- src/lib.rs
+++ src/lib.rs
@@ -29,10 +29,10 @@ impl Display for Error {
             Self::JsonError(error) => error.fmt(f),
             Self::Base64UrlDecodeError(error) => error.fmt(f),
             Self::InvalidKeyLength(error) => error.fmt(f),
-            Self::InvalidHeader => f.write_str("Unsupported header values"),
-            Self::InvalidChecksum => f.write_str("Checksum does not match"),
+            Self::InvalidHeader => f.write_str("unsupported header values"),
+            Self::InvalidChecksum => f.write_str("checksum does not match"),
             Self::MissingPart => f.write_str(
-                "Expected 3 base 64 url encoded parts seperated by dots"
+                "expected 3 base 64 url encoded parts seperated by dots"
             )
         }
     }
@@ -52,10 +52,8 @@ fn decode_part(part: Option<&str>) -> Result<Vec<u8>> {
     }
 }
 
-fn calc_checksum(secret: &str, value: &[u8]) -> Result<Output<Hmac<Sha256>>>{
-    match hmac::Hmac::<Sha256>::new_from_slice(
-        secret.as_bytes()
-    ){
+fn calc_checksum(secret: &[u8], value: &[u8]) -> Result<Output<Hmac<Sha256>>>{
+    match hmac::Hmac::<Sha256>::new_from_slice(secret){
         Ok(mut hasher) => {
             hasher.update(value);
             Ok(hasher.finalize())
@@ -64,8 +62,34 @@ fn calc_checksum(secret: &str, value: &[u8]) -> Result<Output<Hmac<Sha256>>>{
     }
 }
 
-pub fn parse<T>(secret: &str, token: &str) -> Result<T>
-    where T: Serialize + for<'a> Deserialize<'a> {
+/// Deserialize the claims struct from a jwt token
+///
+/// #Example
+///
+/// ```
+/// use jwt_hmac;
+/// use serde::Deserialize;
+///
+/// #[derive(Deserialize)]
+///  struct Claims{
+///     sub: String
+///  }
+///
+///  fn main(){
+///     let secret = "I'm a secret".as_bytes();
+///     match jwt_hmac::parse::<Claims>(
+///         secret,
+///         "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.azXPRJHeWcZ_B5WHtA98gsnowX5gifvMJX2hoH_4YPs"
+///     ){
+///         Ok(claims) => println!("Sub: {}", claims.sub),
+///         Err(error) => match error{
+///             jwt_hmac::Error::InvalidChecksum => println!("Secret doesn't match"),
+///             _ => println!("Probably not a valid JWT: {}", error)
+///         }
+///     }
+///  }
+/// ```
+pub fn parse<T>(secret: &[u8], token: &str) -> Result<T> where T: for<'a> Deserialize<'a> {
     let mut parts = token.split('.');
     match parts.next(){
         Some(header) => {
@@ -88,8 +112,35 @@ pub fn parse<T>(secret: &str, token: &str) -> Result<T>
     }
 }
 
-pub fn create<T>(secret: &str, claims: &T) -> Result<String>
-    where T: Serialize + for<'a> Deserialize<'a> {
+/// Generate a JWT token with the provided claims using the given secret
+///
+/// #Example
+///
+/// ```
+/// use jwt_hmac;
+/// use serde::Serialize;
+///
+/// #[derive(Serialize)]
+///  struct Claims{
+///     sub: String,
+///     name: String,
+///     admin: bool
+///  }
+///
+///  fn main(){
+///     let secret = "I'm a secret".as_bytes();
+///     let claims = Claims{
+///         sub: "1234567890".to_string(),
+///         name: "John Doe".to_string(),
+///         admin: true
+///     };
+///     match jwt_hmac::create(secret, &claims) {
+///         Ok(token) => println!("Token: {}", token),
+///         Err(error) => println!("This can't be happening {}", error)
+///     }
+///  }
+/// ```
+pub fn create<T>(secret: &[u8], claims: &T) -> Result<String> where T: Serialize {
     match serde_json::to_string(claims){
         Ok(json) => Ok(format!(
             "{}.{}.{}",
@@ -105,21 +156,55 @@ pub fn create<T>(secret: &str, claims: &T) -> Result<String>
 
 #[cfg(test)]
 mod tests {
-    use crate::{create, parse};
+    use crate::{create, parse, Error};
     use serde::{Deserialize, Serialize};
 
-    #[derive(Serialize, Deserialize)]
-    struct Test{
-        value: String
+    static SERIALIZED: &str = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.azXPRJHeWcZ_B5WHtA98gsnowX5gifvMJX2hoH_4YPs";
+    static BAD_CHECKSUM: &str = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.azXPRJHeWcZ_B5aHtA98gsnowX5gifvMJX2hoH_4YPs";
+    static NAME: &str = "John Doe";
+    static SECRET: &str = "I'm a secret";
+
+    #[derive(Serialize)]
+    struct OutClaims{
+        sub: String,
+        name: String,
+        admin: bool
+    }
+
+    #[derive(Deserialize)]
+    struct InClaims{
+        name: String
     }
 
     #[test]
-    fn it_works() {
-        let secret = "I'm a secret";
-        let test = Test{
-            value: "Testing".to_string()
+    fn can_create() {
+        let test = OutClaims{
+            sub: "1234567890".to_string(),
+            name: NAME.to_string(),
+            admin: true
         };
-        let token = create(secret, &test).unwrap();
-        assert_eq!(parse::<Test>(secret, &token).unwrap().value, test.value);
+        match create(SECRET.as_bytes(), &test){
+            Ok(token) => assert_eq!(token.as_str(), SERIALIZED),
+            Err(error) => panic!("{}", error)
+        }
+    }
+
+    #[test]
+    fn can_parse(){
+        match parse::<InClaims>(SECRET.as_bytes(), SERIALIZED){
+            Ok(claims) => assert_eq!(claims.name, NAME),
+            Err(error) => panic!("{}", error)
+        }
+    }
+
+    #[test]
+    fn recognizes_bad_checksum(){
+        match parse::<InClaims>(SECRET.as_bytes(), BAD_CHECKSUM){
+            Ok(_) => assert!(false, "Should not recognize checksum"),
+            Err(error) => match error {
+                Error::InvalidChecksum => return,
+                _ => assert!(false, "Should have produced Error::InvalidChecksum")
+            }
+        }
     }
 }
